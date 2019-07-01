a4b68bc7d4ccfcef03f39b600936fc198f3dbc80
greddy36 <gujjugurunadhareddy@gmail.com>
2019-07-01 11:25:20 +0530

    hello2

---
+++ 02variable_size.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Size of char variable is " << sizeof(char) <<" bytes"<<endl;
+	cout << "Size of unsigned char variable is " << sizeof(unsigned char) <<" bytes"<<endl;
+	cout << "Size of signed char variable is " << sizeof(signed char)<<" bytes"<<endl;
+	cout << "Size of int variable is "<<sizeof(int)<<" bytes"<<endl;
+	cout <<"Size of unsigned int variable is "<<sizeof(unsigned int)<<" bytes"<<endl;
+	cout <<"Size of signed int variable is "<<sizeof(signed int)<<" bytes"<<endl;
+	cout <<"Size of short int variable is "<<sizeof(short int)<<" bytes"<<endl;
+	cout <<"Size of signed long int variable is "<<sizeof(signed long int)<<endl;
+	cout <<"Size of unsigned long int variable is "<<sizeof(unsigned long int)<<endl;
+	cout <<"Size of float variable is "<< sizeof(float)<<endl;
+	cout <<"Size of double variable is "<< sizeof(double)<<endl;
+	cout <<"Size of wchar_t variable is "<< sizeof(wchar_t)<<endl;
+}
+
